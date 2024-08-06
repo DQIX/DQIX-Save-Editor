d2fff61f3cd5f81ef61253dba3fba9f9af3bae52
Adenine <adenine.dev@gmail.com>
2024-08-06 10:59:48 -0400

    fix number interactions

--- src/components/atoms/Input.jsx
+++ src/components/atoms/Input.jsx
@@ -1,5 +1,27 @@
+import { useState } from "react"
 import "./Input.scss"
 
 export default props => {
-  return <input {...props} />
+  if (props.type == "number") {
+    let [value, setValue] = useState(props.value)
+
+    return (
+      <input
+        {...props}
+        value={value}
+        onChange={e => {
+          setValue(e.target.value)
+        }}
+        onBlur={e => {
+          if (value === undefined || value === "") {
+            setValue(props.value)
+          } else {
+            props.onChange && props.onChange(e)
+          }
+        }}
+      />
+    )
+  } else {
+    return <input {...props} />
+  }
 }
