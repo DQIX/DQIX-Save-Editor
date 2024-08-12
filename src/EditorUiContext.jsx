1b4b486ef6ec83b30854153aa7feec4e60f549d2
Adenine <adenine.dev@gmail.com>
2024-08-12 07:31:50 -0400

    pwa stuff

--- src/EditorUiContext.jsx
+++ src/EditorUiContext.jsx
@@ -9,7 +9,9 @@ import GrottoEditor from "./components/editorTabs/GrottoEditor"
 import QuestEditor from "./components/editorTabs/QuestEditor"
 
 export const DARK_THEME_NAME = "dark"
+const DARK_THEME_BG = "#11111b"
 export const LIGHT_THEME_NAME = "light"
+const LIGHT_THEME_BG = "#dce0e8"
 
 export const tabs = [
   { name: "party", component: CharacterEditor },
@@ -28,6 +30,9 @@ const initialTheme =
   localStorage.getItem("theme") ||
   (window.matchMedia("(prefers-color-scheme: light)").matches ? LIGHT_THEME_NAME : DARK_THEME_NAME)
 document.documentElement.setAttribute("data-theme", initialTheme)
+document
+  .querySelector('meta[name="theme-color"]')
+  .setAttribute("content", initialTheme == LIGHT_THEME_NAME ? LIGHT_THEME_BG : DARK_THEME_BG)
 
 export const LOAD_STATE_UNLOADED = 0
 export const LOAD_STATE_LOADING = 1
@@ -52,6 +57,9 @@ export const useEditorUiContext = () => {
     setTheme: theme => {
       localStorage.setItem("theme", theme)
       document.documentElement.setAttribute("data-theme", theme)
+      document
+        .querySelector('meta[name="theme-color"]')
+        .setAttribute("content", theme == LIGHT_THEME_NAME ? LIGHT_THEME_BG : DARK_THEME_BG)
       setState({ ...state, theme })
     },
     setTab: tab => {
